9f2d393bef2effabebcc650d1ab4f75c566ae36f
John Easter <john@owltestprep.com>
2020-07-05 03:04:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,6 @@
 def using_push(array = my_array, string = "addrienne")
   my_array = ["Eric", "Kathryn", "John", "Seth"]
   array.push(string)
+  puts using_push(my_array, "addrienne")
 end
 
-puts using_push(my_array, "addrienne")
